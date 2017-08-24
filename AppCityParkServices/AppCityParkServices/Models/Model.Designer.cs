1262009c1da553dee0ac33ba10a1955b7573759b
Brian Molina <bmolina@digitalstrategy.com.ec>
2017-08-24 18:00:12 -0500

    Conectado a una nueva base de datos en SOMEE

--- AppCityParkServices/AppCityParkServices/Models/Model.Designer.cs
+++ AppCityParkServices/AppCityParkServices/Models/Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\yanelys darzon\Source\Repos\AppCityParkServices\AppCityParkServices\Models\Model.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\brian\source\repos\CityParkServicios\AppCityParkServices\AppCityParkServices\Models\Model.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
