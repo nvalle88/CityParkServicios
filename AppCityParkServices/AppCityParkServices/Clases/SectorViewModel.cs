d695c5c8e76085f28c6a0ae0e0351996466cf57d
Brian Molina <bmolina@digitalstrategy.com.ec>
2017-11-28 11:56:49 -0500

    cambios brian

---
+++ AppCityParkServices/AppCityParkServices/Clases/SectorViewModel.cs
@@ -0,0 +1,14 @@
+﻿using AppCityParkServices.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AppCityParkServices.Clases
+{
+    public class SectorViewModel
+    {
+        public Sector Sector { get; set; }
+        public List<PuntoSector> PuntoSector { get; set; }
+    }
+}
